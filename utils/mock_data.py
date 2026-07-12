8c118498a48f12c4a3e98cdf558756a521789777
Divyansh Dubey <divyanshdubey3110@gmail.com>
2026-07-12 11:58:26 +0530

    hour 2 work update

--- utils/mock_data.py
+++ utils/mock_data.py
@@ -1,1 +1,17 @@
+import random
 
+def get_dashboard_data():
+
+    return {
+        "vehicle_count": random.randint(120, 220),
+
+        "traffic_status": random.choice([
+            "Low",
+            "Moderate",
+            "High"
+        ]),
+
+        "average_speed": random.randint(30, 70),
+
+        "alerts": random.randint(0, 5)
+    }
